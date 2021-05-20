42c6c71a2cdae84036000e494441f9a594e0e347
Aashay Pilivkar <aashay13@gmail.com>
2021-05-20 08:24:46 +0530

    Project Upload

---
+++ src/Test_Package/TestClass.java
@@ -0,0 +1,10 @@
+package Test_Package;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git repository");
+	}
+
+}
