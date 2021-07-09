8174fadcaa061ed24398bb94add91829d733c8b8
Aashay Pilivkar <aashay13@gmail.com>
2021-07-09 08:09:04 +0530

    Aashay_push_branch

---
+++ src/package2/Class2.java
@@ -0,0 +1,11 @@
+package package2;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2");
+		System.out.println("2.1");
+	}
+
+}
